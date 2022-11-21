85469a995a22690a4fe431c5c8e0a02916d4692e
Salo <salomonco@gmail.com>
2022-11-21 17:39:31 -0500

    api logic works

--- jokes-app/src/Jokes.js
+++ jokes-app/src/Jokes.js
@@ -1,15 +1,17 @@
 import React, {useState} from 'react'
-
+import './App.css'
 function Jokes(){
   
     const [data, setData] = useState("");
-    const [category, setCategory] = useState(null)
+    const [category, setCategory] = useState('Any')
+    const [string, setString] = useState(null)
+    const [number, setNumber] = useState('1')
 
   // -----------------------------------
 
     async function getJoke(){
 
-        const path = `https://v2.jokeapi.dev/joke/Any?type=single`
+        const path = `https://v2.jokeapi.dev/joke/${category}?type=single&contains=${string}`;
         const res = await fetch(path)
         const json = await res.json()
         const joke = json.joke
@@ -22,17 +24,28 @@ function Jokes(){
   // -----------------------------------
 
   return (
-    <div>
+    <div className="App">
       <h1>{data}</h1>
       <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        getJoke()
-      }}>
-
-      <button type='submit'>Get Joke</button>
-
-
+        onSubmit={(e) => {
+          e.preventDefault();
+          getJoke();
+        }}
+      >
+        <p>Select Category:</p>
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="Any">Any</option>
+          <option value="Programming">Programming</option>
+          <option value="Misc">Misc</option>
+          <option value="Dark">Dark</option>
+        </select>
+        <p>Search for a joke that contains this search string:</p>
+        <input
+          placeholder="(optional)"
+          value={string}
+          onChange={(e) => setString(e.target.value)}
+        />
+        <button type="submit">Get Joke</button>
       </form>
     </div>
   );
